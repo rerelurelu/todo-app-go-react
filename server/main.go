59bbadb392523e17fad93e16bd9ea7d7d9335985
zoniha <awaawapurin@gmail.com>
2022-10-11 16:50:52 +0900

    feat: Add cors settings

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -15,6 +16,11 @@ type Todo struct {
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
